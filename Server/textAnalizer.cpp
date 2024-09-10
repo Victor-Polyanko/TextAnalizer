511d709791b893988bcf164fda5c280cfa624d25
Victor Polyanko <victor.polyanko@envisiontec.de>
2024-09-10 14:29:48 +0300

    Calculate number of unique words in text

--- Server/textAnalizer.cpp
+++ Server/textAnalizer.cpp
@@ -2,6 +2,8 @@
 #include <qjsondocument.h>
 #include <QJsonObject>
 
+const char CONTRACTION = '\'';
+
 TextAnalizer::TextAnalizer()
 {
 }
@@ -10,32 +12,47 @@ QByteArray TextAnalizer::analize(const QByteArray& aInput)
 {
     QString text = QString::fromUtf8(aInput);
     size_t numberOfWords = 0;
-    bool isWord = false;
     bool isContraction = false;
+    QString currentWord;
+    QSet<QString> uniqueWords;
+    auto moveCurrentWord = [&]() {
+        if (uniqueWords.contains(currentWord))
+            currentWord.clear();
+        else
+            uniqueWords.insert(std::move(currentWord));
+    };
     for (auto &symbol : text)
-        if (isWord)
+        if (currentWord.isEmpty())
         {
             if (symbol.isLetter())
-                continue;
-            if (symbol == '\'')
             {
-                if (isContraction)
-                    isWord = false;
-                isContraction = isWord;
+                currentWord.append(symbol);
+                ++numberOfWords;
             }
-            else
-                isWord = false;
         }
         else
         {
             if (symbol.isLetter())
             {
-                isWord = true;
-                ++numberOfWords;
+                if (isContraction)
+                {
+                    isContraction = false;
+                    currentWord.append(CONTRACTION);
+                }
+                currentWord.append(symbol);
             }
+            else if (symbol == CONTRACTION)
+            {
+                if (isContraction)
+                    moveCurrentWord();
+                isContraction = !isContraction;
+            }
+            else
+                moveCurrentWord();
         }
     QJsonObject obj;
     obj["number"] = QString::number(numberOfWords);
+    obj["unique"] = QString::number(uniqueWords.size());
     QJsonDocument doc(obj);
     return doc.toJson();
 }
